263d9f687363d77a560aa106420cc80c6073f2f0
Tristan Davis <Tristandavis888@gmail.com>
2024-02-29 01:23:15 -0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Tristan Davis on 2024-02-29.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
