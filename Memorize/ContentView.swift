75ef60ac9ddd40660db5554382d594a4a84f85ee
Tristan Davis <Tristandavis888@gmail.com>
2024-03-02 16:36:59 -0800

    Created Card

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack() {
+            CardView(isFaceUp: true)
+            CardView(isFaceUp: false)
+            CardView(isFaceUp: true)
+        }
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).strokeBorder(lineWidth: 2.0)
+                Text("🐼")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).strokeBorder(lineWidth: 2.0).fill(.gray)
+            }
         }
         .padding()
     }
 }
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
